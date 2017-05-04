17d59e0e3c98bbfbb6773c5aa38edd30dbae0b0d
irynaperekrest <irina.perekrest@gmail.com>
2017-05-04 19:14:22 +0300

    JS_homework_1

---
+++ JS_1.1/JS/script.js
@@ -0,0 +1,18 @@
+function pow(x, n) {
+    var x = prompt('Enter x value');
+    var n = prompt('Enter n value');
+    var result = 1;
+    for (i = 0; i < Math.abs(n); i++) {
+        result *= x;
+
+    }
+
+
+    if (n > 0) {
+        console.log(result);
+    } else {
+        console.log(1 / result);
+    }
+}
+
+pow();
