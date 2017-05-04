b31f518420f5009b29edfbfc7a9595e7e4a82380
irynaperekrest <irina.perekrest@gmail.com>
2017-05-04 19:42:20 +0300

    Update script.js
    added empty strings

--- JS_1.2/JS/script.js
+++ JS_1.2/JS/script.js
@@ -1,12 +1,12 @@
 var nameList = [];
 
 for (i = 0; i < 5; i++) {
-    nameList[i] = prompt('Enter a name');
+    nameList[i] = prompt('Enter a name', '');
 }
 
 console.log(nameList);
 
-var yourName = prompt('Please enter your name');
+var yourName = prompt('Please enter YOUR name', '');
 var hit = [];
 
 for (i = 0; i < nameList.length; i++) {
